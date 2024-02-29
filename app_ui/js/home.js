aaf8d8dfbe955e29dfff6c26d9fe7f7b7d013a56
ektad93 <ektadeshpande93@gmail.com>
2024-02-29 16:11:32 -0600

    complete adding bar and line graph to dashboard

--- app_ui/js/home.js
+++ app_ui/js/home.js
@@ -1,3 +1,81 @@
+$(document).ready(function() {
+//line graph
+var options = {
+    chart: {
+        type: 'line'
+    },
+    series: [{
+        name: 'sales',
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+    }],
+    xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    }
+}
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
+
+//column bar chart
+var options = {
+    chart: {
+        type: 'bar',
+       
+        toolbar: {
+            show: true
+        }
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded'
+        },
+    },
+    dataLabels: {
+        enabled: true,
+        formatter: function (val) {
+            return val;
+        },
+        offsetY: -20,
+        style: {
+            fontSize: '12px',
+            colors: ["#304758"]
+        }
+    },
+    stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+    },
+    series: [{
+        name: 'Sales',
+        data: [30, 40, 45, 50, 49, 60, 70, 91, 125]
+    }],
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+    },
+    yaxis: {
+        title: {
+            text: 'Number of Sales'
+        }
+    },
+    fill: {
+        opacity: 1
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return val + " sales"
+            }
+        }
+    }
+}
+
+var chart = new ApexCharts(document.querySelector("#chart1"), options);
+
+chart.render();
 
 //filter
    function applyFilters() {
@@ -36,3 +114,4 @@ function displayFilteredData(data) {
     // Show the data grid
     document.getElementById('dataGrid').style.display = 'block';
 }
+});
